8067beb4998f2a1f6f6408b80f55b593c6c4a340
ernesah <ernesa.haxhijaj@gmail.com>
2025-05-03 18:48:26 +0200

    feat(plugin): generate @91life/nx-mf-vite plugin

---
+++ libs/@91life/nx-mf-vite/README.md
@@ -0,0 +1,11 @@
+# nx-mf-vite
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build nx-mf-vite` to build the library.
+
+## Running unit tests
+
+Run `nx test nx-mf-vite` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/@91life/nx-mf-vite/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: '@91life/nx-mf-vite',
+  preset: '../../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/libs/@91life/nx-mf-vite',
+};
