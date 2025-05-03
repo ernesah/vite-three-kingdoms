c69fcb6771c49e462506c26f9299f69ce3291aae
ernesah <ernesa.haxhijaj@gmail.com>
2025-05-03 20:41:15 +0200

    feat(borealis): configure borealis app as MF remote

--- apps/borealis/vite.config.ts
+++ apps/borealis/vite.config.ts
@@ -1,42 +1,25 @@
-/// <reference types='vitest' />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
-import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
+import federation from '@originjs/vite-plugin-federation';
 
-export default defineConfig(() => ({
-  root: __dirname,
-  cacheDir: '../../node_modules/.vite/apps/borealis',
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: 'borealis',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './Component': './src/RemoteComponent.tsx',
+      },
+      shared: ['react', 'react-dom'],
+    }),
+  ],
   server: {
-    port: 4200,
-    host: 'localhost',
+    port: 4202,
   },
-  preview: {
-    port: 4300,
-    host: 'localhost',
-  },
-  plugins: [react(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
   build: {
-    outDir: '../../dist/apps/borealis',
-    emptyOutDir: true,
-    reportCompressedSize: true,
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false,
   },
-  test: {
-    watch: false,
-    globals: true,
-    environment: 'jsdom',
-    include: ['{src,tests}/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    reporters: ['default'],
-    coverage: {
-      reportsDirectory: '../../coverage/apps/borealis',
-      provider: 'v8' as const,
-    },
-  },
-}));
+});
