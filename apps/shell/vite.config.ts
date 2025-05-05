2da07fa2fd4d86ae3d0e7a8e6743b36cf1290856
ernesah <ernesa.haxhijaj@gmail.com>
2025-05-05 22:46:14 +0200

    chore(scripts): remove start:shell script and add build:all and preview:all scripts

--- apps/shell/vite.config.ts
+++ apps/shell/vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import federation from '@originjs/vite-plugin-federation';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
+    tsconfigPaths(),
     react(),
     federation({
       name: 'shell',
@@ -13,13 +15,15 @@ export default defineConfig({
         borealis: 'http://localhost:4202/assets/remoteEntry.js',
         cygnus: 'http://localhost:4203/assets/remoteEntry.js',
       },
-      shared: ['react', 'react-dom'],
+      shared: ['react', 'react-dom', 'styled-components'],
     }),
   ],
   server: {
     port: 4200,
   },
   build: {
+    outDir: '../../dist/apps/shell',
+    assetsDir: 'assets',
     target: 'esnext',
     minify: false,
     cssCodeSplit: false,
